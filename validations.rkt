d70b5c3d559145c509925a7e668846cbb275c8b8
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2020-06-29 09:25:36 -0600

    Validations actualizado
    -Se solucionó el choque con los nombres de funciones entre matrix y validations

--- validations.rkt
+++ validations.rkt
@@ -1,31 +1,31 @@
 #lang racket
 
 (define (win matrix num)
-    (or (horizontal matrix num) (vertical matrix num) (diagonalUpward matrix num) (diagonal matrix num))
+    (or (horizontalValid matrix num) (verticalValid matrix num) (diagonalUpwardValid matrix num) (diagonalValid matrix num))
 )
 
 ;Verifica si un hay una fila llena de num
-(define (horizontal matrix num)
+(define (horizontalValid matrix num)
     (cond
         ((null? matrix) 
             #f
         )
-        ((verifyLine (car matrix) num) 
+        ((verifyLineValid (car matrix) num) 
             #t
         )
         (else
-            (horizontal (cdr matrix) num)
+            (horizontalValid (cdr matrix) num)
         )
     )
 )
 ;Revisa que una linea este llena con num o 0
-(define (verifyLine subList num)
+(define (verifyLineValid subList num)
     (cond
         ((null? subList) 
             #t
         )
         ((equal? (car subList) num) 
-            (verifyLine (cdr subList) num)
+            (verifyLineValid (cdr subList) num)
         )
         (else 
             #f
@@ -33,11 +33,11 @@
     )
 )
 ;Verifica si un hay una columna llena de un numero
-(define (vertical matrix num)
-    (horizontal (transposed matrix) num)
+(define (verticalValid matrix num)
+    (horizontalValid (transposedValid matrix) num)
 )
 ;Transpone la matriz
-(define (transposed matrix)
+(define (transposedValid matrix)
     (cond
         ((null? matrix)
             '()
@@ -46,39 +46,39 @@
             '()
         )
         (else
-            (cons (getColumn matrix) (transposed (deleteColumn matrix)))
+            (cons (getColumnValid matrix) (transposedValid (deleteColumnValid matrix)))
         )
     )
 )
 ;Obtiene la primera columna de una matriz dada
-(define (getColumn matrix)
+(define (getColumnValid matrix)
     (cond
         ((null? matrix)
             '()
         )
         (else
-            (cons (caar matrix) (getColumn (cdr matrix)))
+            (cons (caar matrix) (getColumnValid (cdr matrix)))
         )
     )
 )
 ;Elimina la primera columna de una matriz dada
-(define (deleteColumn matrix)
+(define (deleteColumnValid matrix)
     (cond
         ((or (null? matrix) (null? (car matrix)))
             '()
         )
         (else
-            (cons (cdar matrix) (deleteColumn (cdr matrix)))
+            (cons (cdar matrix) (deleteColumnValid (cdr matrix)))
         )
     )
 )
 ;Obtinen todas las diagonales validas descendentes de la matriz 
-(define (diagonal matrix num)
+(define (diagonalValid matrix num)
     (cond
         ((null? matrix) 
             #f
         )
-        ((or (diagonalUp matrix num) (diagonalDown (cdr matrix) num))
+        ((or (diagonalUpValid matrix num) (diagonalDownValid (cdr matrix) num))
             #t
         )
         (else
@@ -87,53 +87,53 @@
     )
 )
 ;Obtiene las diagonales validas que se encuentran por debajo de la diagonal principal
-(define (diagonalDown matrix num)
+(define (diagonalDownValid matrix num)
     (cond
         ((null? matrix)
             #f
         )
-        ((and (diagonalChecker matrix num) (> (lenghtMatrix matrix) 2) (> (lenghtMatrix (transposed matrix)) 2))
+        ((and (diagonalCheckerValid matrix num) (> (lenghtMatrixValid matrix) 2) (> (lenghtMatrixValid (transposedValid matrix)) 2))
             #t
         )
         (else
-            (diagonalDown (cdr matrix) num)
+            (diagonalDownValid (cdr matrix) num)
         )
     )
 )
 ;Obtiene la diagonal principal de la matriz y las que se encuentran sobre la principal 
-(define (diagonalUp matrix num)
+(define (diagonalUpValid matrix num)
     (cond
         ((null? matrix)
             #f
         )
-        ((and (diagonalChecker matrix num) (> (lenghtMatrix matrix) 2) (> (lenghtMatrix (transposed matrix)) 2))
+        ((and (diagonalCheckerValid matrix num) (> (lenghtMatrixValid matrix) 2) (> (lenghtMatrixValid (transposedValid matrix)) 2))
             #t
         )
         (else
-            (diagonalUp (deleteColumn matrix) num)
+            (diagonalUpValid (deleteColumnValid matrix) num)
         )
     )
 )
 
 ;Obtiene la longitud de la matriz 
-(define (lenghtMatrix matrix)
+(define (lenghtMatrixValid matrix)
     (cond
         ((null? matrix)
             0
         )
         (else
-            (+ 1 (lenghtMatrix (cdr matrix)))
+            (+ 1 (lenghtMatrixValid (cdr matrix)))
         )
     )
 )
 ;Valida que una diagonal contenga a num o 0
-(define (diagonalChecker matrix num)
+(define (diagonalCheckerValid matrix num)
     (cond
         ((or (null? matrix) (null? (car matrix)))
             #t
         )
         ((equal? (caar matrix) num)
-            (diagonalChecker (deleteColumn (cdr matrix)) num)
+            (diagonalCheckerValid (deleteColumnValid (cdr matrix)) num)
         )
         (else
             #f
@@ -141,20 +141,21 @@
     )
 )
 ;Revisa la diagonal ascendente
-(define (diagonalUpward matrix num)
-    (diagonal (invertMatrix matrix) num)
+(define (diagonalUpwardValid matrix num)
+    (diagonalValid (invertMatrixValid matrix) num)
 )
 
 
 ;Invierte la matriz de abajo hacia arriba
-(define (invertMatrix matrix)
+(define (invertMatrixValid matrix)
     (cond
         ((null? matrix) 
             '()
         )
         (else
-            (append (invertMatrix (cdr matrix)) (list (car matrix)))
+            (append (invertMatrixValid (cdr matrix)) (list (car matrix)))
         )
     )
 )
 (provide (all-defined-out))
+;(win '((1 0 0) (1 0 0) (0 1 0)) 1)
