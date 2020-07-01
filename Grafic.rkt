2790e1d46d38fbb3a27ccd6c1ed77a74e69c7bcb
FabianFallas <37458918+FabianFallas@users.noreply.github.com>
2020-07-02 05:54:40 +0600

    Cant-filayColumna
    Se agreaga opcion para elegir

--- Grafic.rkt
+++ Grafic.rkt
@@ -1,8 +1,8 @@
 #lang racket/gui
 
 
-(define M 5)
-(define N 3)
+;(define M 5)
+;(define N 3)
 
 (define dimension_x 600)
 (define dimension_y 600)
@@ -24,6 +24,67 @@
        [label "Tic Tac Toe"]
        [width dimension_x]
        [height (+ dimension_y 59)])); le suma 59 para considerar el espacio de arriba de la ventana
+
+ ; Ventana principal
+( define ventana ( new frame% [ label " Nuevo Juego "]) )
+
+( define datos ( new message%
+                      [ label " Escoja el tamaño de la matriz "]
+                      [ parent ventana ]
+                      [ auto-resize #t ]
+                      ) )
+
+; define ventana q pide al usuario las dimensiones
+( define lanzar
+    ( new button%
+          [ parent ventana ]
+          [ label " escoger "]
+          [ callback
+            ( lambda ( b c )
+               ( send ventana-de-diálogo show #t )
+
+              
+               (define M (string->number (send txt-C get-value)))
+               (define N (string->number (send txt-F get-value) ))
+               
+
+               (cond ((or (> (string->number (send txt-C get-value)) 3) (<  (string->number (send txt-C get-value)) 10) (>  (string->number (send txt-F get-value)) 3) (<  (string->number (send txt-F get-value)) 10)
+                      ( send ventana show #t )))) 
+
+               (and (>= (string->number (send txt-C get-value)) 3) (<=  (string->number (send txt-C get-value)) 10) (>=  (string->number (send txt-F get-value)) 3) (<=  (string->number (send txt-F get-value)) 10)
+                       (send frame show #t) (send ventana show #f))
+              
+               (sleep/yield 1)
+               (draw-vertical-lines dc (/ dimension_y (string->number (send txt-C get-value))));dibuja lineas verticales
+               (draw-horizontal-lines dc (/ dimension_x (string->number (send txt-F get-value) )));dibuja lineas horizontales
+               (create_matrix (string->number (send txt-C get-value)) (string->number (send txt-F get-value)) '());crea la matriz
+               
+            
+               ) ]         
+          ) )
+
+ ; La otra ventana , de diálogo
+( define ventana-de-diálogo ( new dialog% [ label " Escoja las dimensiones del tablero "]) )
+
+( define txt-C ( new text-field%
+                            [ label " columnas :"]
+                            [ parent ventana-de-diálogo ]
+                            ) )
+( define txt-F ( new text-field%
+                              [ label " filas :"]
+                              [ parent ventana-de-diálogo ]
+                              ) )
+( new button%
+      [ parent ventana-de-diálogo ]
+      [ label " Aceptar "]
+      [ callback ( lambda ( b c ) ( send ventana-de-diálogo show #f ) ) ]
+      )
+( send ventana show #t )
+    
+(define M (string->number (send txt-C get-value)))
+(define N (string->number (send txt-F get-value) ))
+
+
 
 ;define el click
 
@@ -85,14 +146,14 @@
 
 ;dibuja X's
 (define (draw_X position_x position_y)
-  (cond (      (= (list-ref (list-ref matrix (quotient position_y (quotient dimension_y M))) (quotient position_x (quotient dimension_x N)) )0 )
+  (cond (      (= (list-ref (list-ref matrix (quotient position_y (quotient dimension_y  (string->number (send txt-C get-value) )))) (quotient position_x (quotient dimension_x  (string->number (send txt-F get-value) ))) )0 )
 
                
-  (draw_X_aux (+ (/ dimension_x (* 2 N))(* (quotient position_x (quotient dimension_x N))(/ dimension_x N))) (+ (/ dimension_y (* 2 M))(* (quotient position_y (quotient dimension_y M))(/ dimension_y M)))  )
+  (draw_X_aux (+ (/ dimension_x (* 2 (string->number (send txt-F get-value))))(* (quotient position_x (quotient dimension_x (string->number (send txt-F get-value))))(/ dimension_x (string->number (send txt-F get-value))))) (+ (/ dimension_y (* 2 (string->number (send txt-C get-value))))(* (quotient position_y (quotient dimension_y (string->number (send txt-C get-value))))(/ dimension_y (string->number (send txt-C get-value)))))  )
 
-  (set! matrix (change-at matrix (quotient position_y (quotient dimension_y M)) (quotient position_x (quotient dimension_x N)) 1  ))
+  (set! matrix (change-at matrix (quotient position_y (quotient dimension_y (string->number (send txt-C get-value)))) (quotient position_x (quotient dimension_x (string->number (send txt-F get-value)))) 1  ))
 
-  (draw_O (+ (/ dimension_x (* 2 N))(* (list-ref(list-ref (list-ref (greedyAlgorithm matrix)0)0)1) (/ dimension_x N))) (+ (/ dimension_y (* 2 M))(* (list-ref(list-ref (list-ref (greedyAlgorithm matrix)0)0)0) (/ dimension_y M)))  )
+  (draw_O (+ (/ dimension_x (* 2 (string->number (send txt-F get-value))))(* (list-ref(list-ref (list-ref (greedyAlgorithm matrix)0)0)1) (/ dimension_x (string->number (send txt-F get-value))))) (+ (/ dimension_y (* 2 (string->number (send txt-C get-value))))(* (list-ref(list-ref (list-ref (greedyAlgorithm matrix)0)0)0) (/ dimension_y (string->number (send txt-C get-value)))))  )
 
   (set! matrix (caadr (greedyAlgorithm matrix)))
 
@@ -110,7 +171,7 @@
 
 ;O's
 (define (draw_O position_x position_y)
-  (draw_O_aux (+ (/ dimension_x (* 2 N))(* (quotient position_x (quotient dimension_x N))(/ dimension_x N))) (+ (/ dimension_y (* 2 M))(* (quotient position_y (quotient dimension_y M))(/ dimension_y M)))  )
+  (draw_O_aux (+ (/ dimension_x (* 2 (string->number (send txt-F get-value))))(* (quotient position_x (quotient dimension_x (string->number (send txt-F get-value))))(/ dimension_x (string->number (send txt-F get-value))))) (+ (/ dimension_y (* 2 (string->number (send txt-C get-value))))(* (quotient position_y (quotient dimension_y (string->number (send txt-C get-value))))(/ dimension_y (string->number (send txt-C get-value)))))  )
   )
 
 (define (draw_O_aux x_center y_center)
@@ -164,8 +225,8 @@
        [callback (lambda (i e) (send frame show #f))]
        [shortcut #\q]))
 
-(send frame show #t)
- (sleep/yield 1)
-(draw-vertical-lines dc (/ dimension_y N));dibuja lineas verticales
-(draw-horizontal-lines dc (/ dimension_x M));dibuja lineas horizontales
-(create_matrix M N '());crea la matriz
+;(send frame show #t)
+; (sleep/yield 1)
+;(draw-vertical-lines dc (/ dimension_y N));dibuja lineas verticales
+;(draw-horizontal-lines dc (/ dimension_x M));dibuja lineas horizontales
+;(create_matrix M N '());crea la matriz
