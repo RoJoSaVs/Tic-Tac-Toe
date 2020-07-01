120b0d0af4733e056a577312c99c042af7d6c666
Calquito <39011234+Calquito@users.noreply.github.com>
2020-06-30 20:44:09 -0600

    now is possible to create an NxM matrix

--- GUI/Grafic.rkt
+++ GUI/Grafic.rkt
@@ -1,10 +1,52 @@
 #lang racket/gui
+
+
+(define M 7)
+(define N 5)
+
+(define dimension_x 600)
+(define dimension_y 600)
+
+
+;__________________________________________________________________________________________________________________________________________________________________________________
+
+
+
 (define frame
   (new frame%
        [label "Tic Tac Toe"]
-       [width 600]
-       [height 600]))
- 
+       [width dimension_x]
+       [height (+ dimension_y 59)])); le suma 59 para considerar el espacio de arriba de la ventana
+
+; define un canvas sobre el frame
+(define canvas (new canvas% [parent frame]))
+(define dc (send canvas get-dc))
+
+
+;dibuja encima del canvas
+
+;lineas verticales
+(define (draw-vertical-lines dc position)
+(cond((< dimension_x position) #f)
+     
+     (else (send dc draw-line position 0 position dimension_x); primero las coordenadas iniciales, luego las finaless
+           (draw-vertical-lines dc (+ position (/ dimension_x M)) )
+
+      )))
+  
+   
+
+
+
+;lineas horizontales
+(define (draw-horizontal-lines dc position)
+(cond((< dimension_y position) #f)
+     
+     (else (send dc draw-line 0 position dimension_y position); primero las coordenadas iniciales, luego las finaless
+           (draw-horizontal-lines dc (+ position (/ dimension_y N)) )
+
+      )))
+
 (define menu-bar
   (new menu-bar%
        [parent frame]))
@@ -29,3 +71,6 @@
        [shortcut #\q]))
 
 (send frame show #t)
+ (sleep/yield 1)
+(draw-vertical-lines dc (/ dimension_x M));dibuja lineas verticales
+(draw-horizontal-lines dc (/ dimension_y N));dibuja lineas horizontales
